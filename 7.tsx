d96dd486a1d5ccb5e6e77f34bb64b409c2619841
budaowenghc <1191717141@qq.com>
2022-04-14 17:00:40 +0800

    Revert "map"
    This reverts commit faf0245c45ae3f98829acdbdf0d4ae1cc76bb631.

--- 7.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="7" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../Desktop/元宇宙办公/tilesets/ddz/7.png" width="32" height="32"/>
-</tileset>
--- MegaPixelArt32x32pxIcons_SpriteSheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="MegaPixelArt32x32pxIcons_SpriteSheet" tilewidth="32" tileheight="32" tilecount="504" columns="14">
- <image source="../../../Desktop/元宇宙办公/tilesets/download1/MegaPixelArt32x32pxIcons_SpriteSheet.png" width="448" height="1152"/>
-</tileset>
